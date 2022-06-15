4a7ad7052395c5d2739eae20e49942865647a928
cardenalcopia <kirisse85@gmail.com>
2022-06-15 12:33:08 +0200

    Subida

---
+++ dia3Mongo.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+const teacherSchema = new mongoose.Schema({
+    teacher_first_name:String,
+   teacher_last_name:String
+  
+});
+const markSchema = new mongoose.Schema({
+   date:Date,
+   mark:Number,
+   student_first_name:String,
+   student_last_name:String,
+   group_name:String,
+   subject_name:String,
+   tachers:[teacherSchema]
+})
+module.exports= mongoose.model("Mark",markSchema)
